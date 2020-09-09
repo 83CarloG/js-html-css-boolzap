75544fabce521536cbc195fd8a3e1e190068e30d
Carlo Giuseppe Colombo <cgcolombo1983@hotmail.com>
2020-09-09 11:37:10 +0200

    css fix main-chat

--- js/script.js
+++ js/script.js
@@ -4,9 +4,9 @@ $(document).ready(function() {
 	$('.input-mex').keypress(function	(e) {
 		if (e.which === 13) {
 			var inputValue = $(this).val();
-			var msgElement = $('.template .messaggio-riga').clone();
+			var msgElement = $('.template .mex-row').clone();
 			if (inputValue !== '') {
-				msgElement.find('.mex-testo').text(inputValue);
+				msgElement.find('.mex__text').text(inputValue);
 				$('.wrap-main-chat').append(msgElement);
 				$(this).val('');
 			}
@@ -15,9 +15,9 @@ $(document).ready(function() {
 
 	$('.send-icon').click(function	(){
 		var inputValue = $('.input-mex').val();
-		var msgElement = $('.template .messaggio-riga').clone();
+		var msgElement = $('.template .mex-row').clone();
 		if (inputValue !== '') {
-			msgElement.find('.mex-testo').text(inputValue);
+			msgElement.find('.mex__text').text(inputValue);
 			$('.wrap-main-chat').append(msgElement);
 			$('.input-mex').val('');
 		};
