4f770650c4088fb3ef864a5484d390a18c784fb3
Carlo Giuseppe Colombo <cgcolombo1983@hotmail.com>
2020-09-09 14:46:31 +0200

    js fix

--- js/script.js
+++ js/script.js
@@ -1,25 +1,28 @@
 
-$(document).ready(function() {
-
+$(document).ready(function	()	{
 	$('.input-mex').keypress(function	(e) {
 		if (e.which === 13) {
-			var inputValue = $(this).val();
-			var msgElement = $('.template .mex-row').clone();
-			if (inputValue !== '') {
-				msgElement.find('.mex__text').text(inputValue);
-				$('.wrap-main-chat').append(msgElement);
-				$(this).val('');
-			}
+			sendMessage();
 		}
 	});
 
-	$('.send-icon').click(function	(){
-		var inputValue = $('.input-mex').val();
-		var msgElement = $('.template .mex-row').clone();
-		if (inputValue !== '') {
-			msgElement.find('.mex__text').text(inputValue);
-			$('.wrap-main-chat').append(msgElement);
-			$('.input-mex').val('');
-		};
+	$('.send-icon').click(function	()	{
+		sendMessage();
 	});
 });
+
+
+function sendMessage ()	{
+	var inputValue = $('.input-mex').val();
+	if (inputValue !== '') {
+		var msgElement = $('.template .mex-row').clone();
+		var date = new Date();
+		var hours = date.getHours();
+		var minutes = date.getMinutes();
+		var time = hours + ':' + minutes;
+		msgElement.find('.mex__text').text(inputValue);
+		msgElement.find('.mex__orario').text(time);
+		$('.wrap-main-chat').append(msgElement);
+		$('.input-mex').val('');
+	}
+}
