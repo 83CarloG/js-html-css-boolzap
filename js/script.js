c0ce39099d1ed09f836d6a0a611805eb3f4ad08e
Carlo Giuseppe Colombo <cgcolombo1983@hotmail.com>
2020-09-10 15:21:00 +0200

    js milestone 2 OK

--- js/script.js
+++ js/script.js
@@ -3,32 +3,30 @@ $(document).ready(function	()	{
 	$('.input-mex').keypress(function	(e) {
 		if (e.which === 13) {
 			sendMessage();
-			setTimeout(answer, 1000);
 		}
 	});
 
 	$('.send-icon').click(function	()	{
 		sendMessage();
-		setTimeout(answer, 1000);
-	});
-	$('.ricerca__input').keyup(function	()	{
-		// Search text
-		var text = $(this).val().toLowerCase();
-		// Hide all content class element
-		$('.amici__item').hide();
-		// Search and show
-		$('.amici__item:contains("' + text + '")').show();
 	});
 
+	// $('.ricerca__input').keyup(function	()	{
+	// 	var text = $(this).val().toLowerCase();
+	// 	$('.amici__item').hide();
+	// 	$('.amici__item:contains("' + text + '")').show();
+	// });
+
 	$('.ricerca__input').keyup(function	()	{
- // Search text
-		var text = $(this).val();
-
- // Hide all content class element
-		$('.amici__item').hide();
-
- // Search and show
-		$('.amici__item:contains("' + text + '")').show();
+		var serchInput = $(this).val().toLowerCase();
+		var contactsName = $('.amici__nome');
+		contactsName.each(function	() {
+			var name = $(this).text().toLowerCase();
+			if (name.includes(serchInput)) {
+				$(this).parents('.amici__item').show();
+			} else {
+				$(this).parents('.amici__item').hide();
+			}
+		});
 	});
 });
 
@@ -36,26 +34,32 @@ function sendMessage ()	{
 	var inputValue = $('.input-mex').val();
 	if (inputValue !== '') {
 		var msgElement = $('.template .mex-row').clone();
-		var date = new Date();
-		var hours = date.getHours();
-		var minutes = date.getMinutes();
-		var time = hours + ':' + minutes;
+		var time = getTime();
 		msgElement.find('.mex__text').text(inputValue);
 		msgElement.find('.mex__orario').text(time);
 		$('.wrap-main-chat').append(msgElement);
 		$('.input-mex').val('');
+		setTimeout(answer, 1000);
 	}
 }
 
 function answer () {
 	var msgElement = $('.template .mex-row').clone();
-	var date = new Date();
-	var hours = date.getHours();
-	var minutes = date.getMinutes();
-	var time = hours + ':' + minutes;
+	var time = getTime();
 	msgElement.removeClass('mex-row--sent');
 	msgElement.children('.mex').removeClass('mex--green');
 	msgElement.find('.mex__text').text('ok');
 	msgElement.find('.mex__orario').text(time);
 	$('.wrap-main-chat').append(msgElement);
+}
+
+function getTime ()	{
+	var date = new Date();
+	var hours = date.getHours();
+	var minutes = date.getMinutes();
+	if (minutes < 10) {
+		minutes = '0' + minutes;
+	}
+	var time = hours + ':' + minutes;
+	return time;
 }
